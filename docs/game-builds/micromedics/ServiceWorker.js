19f886e34a07a67c9222786d841eceb6ea7f0d9e
icarus-2 <ebriffett01@gmail.com>
2026-06-15 12:17:06 -0400

    prod build test

---
+++ docs/game-builds/micromedics/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "EduQlasse-MicroMedics-Liver-1.0";
+const contentToCache = [
+    "Build/micromedicsliver-build.loader.js",
+    "Build/micromedicsliver-build.framework.js.br",
+    "Build/micromedicsliver-build.data.br",
+    "Build/micromedicsliver-build.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
